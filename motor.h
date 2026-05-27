0daecda90cb9bdd1f17f72f785937046baa7760d
Felix Löfgren <felixxlofgren@gmail.com>
2026-05-27 22:42:21 +0100

    Lade till motor_set_id funktion

--- motor.h
+++ motor.h
@@ -22,5 +22,6 @@ void motor_off(Motor *m);
 void motor_set_rpm(Motor *m, uint16_t rpm);
 void motor_error(Motor *m);
 void motor_print(const Motor* m);
+void motor_set_id(Motor *m, uint16_t id);
 
 #endif
